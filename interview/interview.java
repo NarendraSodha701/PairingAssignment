7e67e34ce56e67d0e46e46b4a479fc1251079386
NarendraSodha701 <83528078+NarendraSodha701@users.noreply.github.com>
2021-06-22 11:40:56 +0530

    Add files via upload

---
+++ interview/interview.java
@@ -0,0 +1,64 @@
+
+
+
+merger sort 
+input 
+
+    
+arr1 = {?????3,5,7,9}?????
+
+    
+arr2={?????1,2,4,8,10}?????
+
+
+1,2,3,4,5,7,8,9,10 
+
+
+int arr1 = {?????3,5,7,9}?????;
+
+int arr2={?????1,2,4,8,10}?????;
+
+int a = arr1.length;
+int b = arr2.length;
+
+
+int c = a+b;
+
+int c[] = new int[c];
+
+
+System.arraycopy(arr1,0,c,
+
+Sysout(Arrays
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
